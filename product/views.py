9f56ce7b59f8228b7efc5f99bd25e294cac18192
sherlock237 <achal@bitglaze.com>
2021-04-24 11:58:06 +0530

    achal

--- product/views.py
+++ product/views.py
@@ -3,3 +3,5 @@ from . models import Product
 
     #     return redirect('http://127.0.0.1:8000/ecommerce/sell/')
     # return render(request,'ecommerce/sell.html')
+def indexView(request):
+    return render(request,'index.html')
