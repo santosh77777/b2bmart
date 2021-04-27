be5dbacfde5c62ed6b6f405f777726cb8285a4b7
Santosh K <santoshk43214@gmail.com>
2021-04-27 17:36:36 +0530

    seller single product form update

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -55,7 +55,56 @@ class SellerCompanyForm(forms.ModelForm):
 
 
 class SellerSingleProductViewForm(forms.ModelForm):
+    PRODUCT_GROUP_CHOICES = (
+    ("Kitchen_stoves", "Kitchen Stoves"),
+    ("mixture_grinder", "Mixture Grinder"),
+    ("rice_cookers", "Rice Cookers"),
+    ("food_processors", "Food Processors"),
+    ("electric_mixers", "Electric Mixers"),
+    ("juicers", "Juicers"),
+    ("blenders", "Blenders"),
+    ("water_heaters", "Water Heaters"),
+    ("water_filters", "Water Filters"),
+    ("induction cookers", "Induction Cookers"),
+    ("exhaust_hoods", "Exhaust Hoods"),
+    ("sandwich_makers", "Sandwich Makers"),
+    ("toaster", "Toaster"),
+    ("deep_fryers", "Deep Fryers"),
+    ("dough_blenders", "Dough Blenders"),
+    ("coffee_makers", "Coffee Makers"),
+    ("electric_iron", "Electric Iron"),
+    ("vaccum_cleaner", "Vaccum Cleaner"),
+    ("air_purifiers", "Air Purifiers"),
+    ("trimmers_&_savers", "Trimmer And Savers"),
+    ("hair_drier", "Hair Drier"),
+    )
+    name = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    price = forms.IntegerField(widget=forms.NumberInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    unit_type = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    min_order_quantity = forms.IntegerField(widget=forms.NumberInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    product_group = forms.ChoiceField(initial='Select Value', widget=forms.Select( attrs={'class': 'form-control'}), choices=PRODUCT_GROUP_CHOICES,  required=True)
+    description = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    capacity = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    material = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    brand = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    color = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    size = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    model_no = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    power = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    warranty = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    neck_size = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    closure_type = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    product_code = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    packing_details = forms.CharField(widget=forms.TextInput( attrs={'class': 'form-control', 'id':''}), required=True)
+    video_url = forms.URLField(widget=forms.URLInput(attrs={'class': 'form-control',  'id':""}), required=True)
+    image1 = forms.ImageField(widget=forms.FileInput( attrs={'class': 'form-control'}), required=False)
+    image2 = forms.ImageField(widget=forms.FileInput( attrs={'class': 'form-control'}), required=False)
+    image3 = forms.ImageField(widget=forms.FileInput( attrs={'class': 'form-control'}), required=False)
+
+
     class Meta:
         model = Product
-        fields = "__all__"
-        exclude = ['user']
+        fields = ['name', 'price', 'unit_type', 'min_order_quantity', 'product_group', 'description', 
+        'description', 'capacity','material','brand','color','size', 'model_no', 'power', 'warranty', 'neck_size', 'closure_type', 'product_code', 'packing_details'
+          ,'video_url', 'is_available', 'image1', 'image2', 'image3', 'arrange', 'add_home' ]
+        
