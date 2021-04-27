fd63b78da11f9d549d65a00063217c201f8be750
sherlock237 <achal@bitglaze.com>
2021-04-27 15:17:43 +0530

    achal

--- product/views.py
+++ product/views.py
@@ -3,14 +3,48 @@ from . models import Product
 from dashboard.models import SellerCompany
 from django.views.generic import ListView
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+import json
+id_brand=[]
+def HomeView(request):
+    print("YEs")
+    brand=Product.objects.values_list('brand', flat=True)
+    brand_id=Product.objects.values_list('id',flat=True)
+    brand=json.dumps(list(brand))
+    brand_id=json.dumps(list(brand_id))
+    if request.is_ajax():
+        global id_brand
+        id_brand=request.POST.getlist('brand[]')
+        print(id_brand)
+        return HttpResponse('success') 
+    object_list=Product.objects.filter(add_home=True)
+    context={'brand_data':brand,
+             'brand_id':brand_id,
+             'object_list':object_list
+             }
+    return render(request,'index.html',  context)
+    
+        
+def category(request):
+    print("category")
+    print(id_brand)
+    return render(request,'category.html')
 
-class HomeView(ListView):
-    model = Product
-    template_name = "index.html"
-
-    def get_queryset(self):
-        return Product.objects.filter(add_home=True)
-        # return Product.objects.filter(user=user)
+        
+    # def get_context_data(self, **kwargs):
+            
+    #     # Call the base implementation first to get a context
+    #     context = super().get_queryset(**kwargs)
+        
+    #     # context['brand']=Product.objects.filter('brand', flat=True)
+    #     # context['brand_id']=Product.objects.filter('id',flat=True)
+    #     # print(context)
+        
+    #     # brand=json.dumps(list(brand))
+    #     # brand_id=json.dumps(list(brand_id))
+        
+    #     return context
+    #     # return Product.objects.filter(user=user)
     
 
 class HomeProductList(ListView):
@@ -31,26 +65,3 @@ class HomeProductList(ListView):
         context['seller_company'] = SellerCompany.objects.filter(user=user)
         return context
         
-from django.http import HttpResponse
-import json
-
-    #     return redirect('http://127.0.0.1:8000/ecommerce/sell/')
-    # return render(request,'ecommerce/sell.html')
-id_brand=[]
-def index(request):
-    print("YEs")
-    brand=Product.objects.values_list('brand', flat=True)
-    brand_id=Product.objects.values_list('id',flat=True)
-    brand=json.dumps(list(brand))
-    brand_id=json.dumps(list(brand_id))
-    if request.is_ajax():
-       global id_brand
-       id_brand=request.POST.getlist('brand[]')
-       print(id_brand)
-       return HttpResponse('success') 
-    return render(request,'index.html',{'brand_data':brand,'brand_id':brand_id})
-
-def category(request):
-    print("category")
-    print(id_brand)
-    return render(request,'category.html')
